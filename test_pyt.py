9aef317367d751115caa7f766cc14194d77b6821
Mohit Raghav <79373327+rmohit011@users.noreply.github.com>
2023-09-08 16:47:47 +0530

    Create test_pyt.py

---
+++ test_pyt.py
@@ -0,0 +1,32 @@
+import pytest
+
+# Fixture: Setting up test data
+@pytest.fixture
+def sample_data():
+    return 5, 3
+
+# Test cases for addition
+def test_addition(sample_data):
+    x, y = sample_data
+    result = x+y
+    assert result == 8
+
+# Test cases for subtraction
+def test_subtraction(sample_data):
+    x, y = sample_data
+    result = x-y
+    assert result == 2
+
+# Test cases for multiplication
+def test_multiplication(sample_data):
+    x, y = sample_data
+    result = x*y
+    assert result == 15
+
+# Test cases for division
+def test_division(sample_data):
+    x, y = sample_data
+    result = x/y
+    assert result == 5/3
+
+
