3e38beedf007c5f55023d80565718928ff1df82f
Mohit Raghav <79373327+rmohit011@users.noreply.github.com>
2023-09-09 12:24:39 +0530

    Update test_cml.py

--- test_cml.py
+++ test_cml.py
@@ -4,4 +4,4 @@ def test_environment(name):
     assert name in ["dev", "staging", "prod"], "Invalid environment specified"
 
 def test_exec(name):
-    assert name == 'staging', 'name invalid'
+    assert name == 'dev', 'name invalid'
